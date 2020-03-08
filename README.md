76ea795648e0ab5fb32aabe078414b9a84197e54
HBY <1534512408@qq.com>
2020-03-08 17:26:49 +0800

    更新

--- README.md
+++ README.md
@@ -2,4 +2,7 @@
 
 一个使用restful风格的flask api
 
+/logs 是通过request.args获取参数  
+/log 是通过json获取参数
+
 api文档：https://www.showdoc.cc/683002674729729
--- app/api/v1_0/views.py
+++ app/api/v1_0/views.py
@@ -15,7 +15,7 @@ api = Api(app)
 
 
 class Logs(Resource):
-    # 增添备忘录，获取前端发送的json
+    # 增添备忘录，获取前端发送的参数
     # info:文本内容
     # deadtime:截至时间
     def post(self):
@@ -136,7 +136,7 @@ class Logs(Resource):
             return jsonify({"status": 200, "data": lit})
 
     def delete(self):
-        if "class" not in request.args["class"]:
+        if "class" not in request.args:
             return jsonify({"status":4001})
         data = {
             "class": eval(request.args["class"]),
@@ -194,26 +194,54 @@ class Logs(Resource):
 
 class Log(Resource):
     # 增添备忘录，获取前端发送的json
+    # info:文本内容
+    # deadtime:截至时间
     def post(self):
-        data = json.loads(request.get_data(as_text=True))
-        for key, value in data.items():
-            if value == '':
-                data[key] = 0
+        data=json.loads(request.get_data(as_text=True))
+        if "info" not in data or "deadtime" not in data:
+            return jsonify({"status": 4001})
+        try:
+            tm = datetime.datetime.strptime(data["deadtime"], "%Y-%m-%d %H:%M:%S")
+        except:
+            return jsonify({"status": 4002})
         lg = log(
             info=data['info'],
             ok=0,
-            deadtime=datetime.datetime.strptime(data["deadtime"], "%Y-%m-%d %H:%M:%S")
+            deadtime=tm
         )
         db.session.add(lg)
         db.session.commit()
-        return jsonify({"status": "200"})
+        return jsonify({"status": 200})
 
     def put(self):
         data = json.loads(request.get_data(as_text=True))
+        if "class" not in data or "op" not in data:
+            return jsonify({"status": 4001})
+        try:
+            data = {
+                "class": eval(data["class"]),
+                "op": eval(data["op"]),
+                "id": 0
+            }
+        except:
+            return jsonify({"status": 4002})
+        if data["class"] != 1 and data["class"] != 2:
+            return jsonify({"status": 4002})
+        if data["op"] != 1 and data["op"] != 0:
+            return jsonify({"status": 4002})
+
         # 一条
         if data["class"] == 1:
-            id = data["id"]
+            if "id" not in data:
+                return jsonify({"status": 4001})
+            try:
+                id = eval(data["id"])
+            except:
+                return jsonify({"status": 4002})
+            print(id)
             lg = log.query.filter_by(id=id).first()
+            if lg == None:
+                return jsonify({"status": 4003})
             # 一条-->已完成
             if data["op"] == 1:
                 lg.ok = 1
@@ -239,8 +267,21 @@ class Log(Resource):
                 db.session.commit()
 
     def get(self):
-        # 查询操作
         data = json.loads(request.get_data(as_text=True))
+        if "op" not in data or "class" not in data:
+            return jsonify({"status": 4001})
+        # 查询操作
+        try:
+            data = {
+                "op":data["op"],
+                "class": data["class"]
+            }
+        except:
+            return jsonify({"status": 4002})
+        if data["class"] != 1 and data["class"] != 2 and data["class"] != 0:
+            return jsonify({"status": 4002})
+        if data["op"] != 1 and data["op"] != 0:
+            return jsonify({"status": 4002})
         # 查询数目
         if data["op"] == 0:
             num = 0
@@ -250,7 +291,7 @@ class Log(Resource):
                 num = log.query.filter_by(ok=0).count()
             if data["class"] == 2:
                 num = log.query.filter_by(ok=0).count() + log.query.filter_by(ok=1).count()
-            return jsonify({"status": "200", "num": num})
+            return jsonify({"status": 200, "num": num})
         if data["op"] == 1:
             lit = []
             if data["class"] == 1:
@@ -269,16 +310,17 @@ class Log(Resource):
                        "deadtime": str(lg.deadtime)
                        }
                 lit.append(jsn)
-            return jsonify({"status": "200", "data": lit})
+            return jsonify({"status": 200, "data": lit})
 
     def delete(self):
         data = json.loads(request.get_data(as_text=True))
-        flag = 1
-        for key in data:
-            if key == "class":
-                flag = 0
-        if flag:
-            return jsonify({"status": "402"})
+        if "class" not in data:
+            return jsonify({"status": 4001})
+        data = {
+            "class": eval(data["class"]),
+        }
+        if data["class"] < 0 or data["class"] > 3:
+            return jsonify({"status": 4002})
         # 全删
         if data["class"] == 0:
             logs = log.query.order_by(
@@ -287,7 +329,7 @@ class Log(Resource):
             for it in logs:
                 db.session.delete(it)
                 db.session.commit()
-            return jsonify({"status": "200"})
+            return jsonify({"status": 200})
         # 删已完成
         if data["class"] == 1:
             logs = log.query.order_by(
@@ -298,7 +340,7 @@ class Log(Resource):
                     continue
                 db.session.delete(it)
                 db.session.commit()
-            return jsonify({"status": "200"})
+            return jsonify({"status": 200})
         # 删未完成
         if data["class"] == 2:
             logs = log.query.order_by(
@@ -309,19 +351,24 @@ class Log(Resource):
                     continue
                 db.session.delete(it)
                 db.session.commit()
-            return jsonify({"status": "200"})
+            return jsonify({"status": 200})
         # 删一条
         if data["class"] == 3:
-            id = data["id"]
+            id = eval(data["id"])
             print(id)
             lg = log.query.filter_by(id=id).first()
             if lg == None:
-                return jsonify({"status": "404"})
+                return jsonify({"status": 4003})
             db.session.delete(lg)
             db.session()
-            return jsonify({"status": "200"})
-        return jsonify({"status": "403"})
+            return jsonify({"status": 200})
+        return jsonify({"status": 4002})
 
 
 api.add_resource(Log, "/log", endpoint='log')
 api.add_resource(Logs, "/logs", endpoint='logs')
+
+
+@app.route("/")
+def index():
+    return render_template("test.html")
